1c938c0a0cff56a185c76ee0fb1851ca665aacf7
Masooma_khan <k.masooma198@gmail.com>
2024-09-10 17:43:19 +0500

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,27 @@
+/*import 'package:calculator/calculator.dart';
+import 'package:flutter/material.dart';
+
+
+//Stateless widget
+
+void main() {
+  runApp(const MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter learn",
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home:  const CalculatorView()
+    );
+  }
+}*/
---
+++ lib/custom_text widget.dart
@@ -0,0 +1,11 @@
+/*import 'package:flutter/material.dart' show BuildContext, StatelessWidget, Text, Widget;
+
+class CustomTextWidget extends StatelessWidget {
+  final String text;
+   const CustomTextWidget(this.text, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text);
+  }
+}*/
